8407822adf4c840f1677ed5fa794671f22b62d8e
John-Patrick-Russel-Lalo <johnpatricklalo@gmail.com>
2024-12-13 07:56:13 +0800

    Add files via upload

---
+++ InventoryManagement.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+class Inventory_Manager:
+    def __init__(self, inventory_file_path):
+        self.inventory_file_path = inventory_file_path
+        self.inventory = self.load_inventory()
+
+    def load_inventory(self):
+        if os.path.exists(self.inventory_file_path):
+            with open(self.inventory_file_path, 'r') as f:
+                return json.load(f)
+        return {}
+
+    def save_inventory(self):
+        with open(self.inventory_file_path, 'w') as f:
+            json.dump(self.inventory, f)
+
+    def add_item(self, item_name, quantity, price):
+        if item_name in self.inventory:
+            self.inventory[item_name]['quantity'] += quantity
+            self.inventory[item_name]['price'] = price
+        else:
+            self.inventory[item_name] = {'quantity': quantity, 'price': price}
+        self.save_inventory()
+
+    def remove_item(self, item_name):
+        if item_name in self.inventory:
+            del self.inventory[item_name]
+            self.save_inventory()
+            return True
+        return False
